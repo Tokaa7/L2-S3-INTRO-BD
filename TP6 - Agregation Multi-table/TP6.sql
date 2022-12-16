018990ac15c24a058cd6dbb523f0c90f5e3d5681
Tokaa7 <ksoares@outlook.fr>
2022-12-16 22:14:34 +0100

    Update TP6.sql

--- TP6 - Agregation Multi-table/TP6.sql
+++ TP6 - Agregation Multi-table/TP6.sql
@@ -9,7 +9,7 @@ SELECT MAX(Remise) FROM détails_commandes;
 SELECT SUM(détails_commandes.Quantité) AS SommeQuantité FROM détails_commandes, commandes, clients
 WHERE Réfcommande = N°commande AND Refclient = Codeclient AND Société="QUICK-stop" AND YEAR(Date_commande)=2018;
 
-SELECT Société, SUM(quantité*PU) FROM Détails_commandes, Commandes, Clients
+SELECT Société, SUM(quantité*PU) AS SommeCommandes FROM Détails_commandes, Commandes, Clients
 WHERE Réfcommande = N°commande AND Refclient = Codeclient
 AND Société = "Hanari Carnes";
 
@@ -17,19 +17,19 @@ AND Société = "Hanari Carnes";
 
 SELECT Nom_catégorie, COUNT(*) AS SommeProduit FROM produits INNER JOIN catégories ON Refcatégorie = code_catégorie GROUP BY Nom_catégorie;
 
-SELECT Pays, COUNT(*) FROM Clients GROUP BY Pays;
+SELECT Pays, COUNT(*) AS NbClients FROM Clients GROUP BY Pays;
 
-SELECT Société, COUNT(*) FROM Clients INNER JOIN Commandes ON Codeclient = Refclient GROUP BY Société;
+SELECT Société, COUNT(*) AS NbCommandes FROM Clients INNER JOIN Commandes ON Codeclient = Refclient GROUP BY Société;
 
-SELECT Nom_catégorie, MIN(PU), MAX(PU), AVG(PU) FROM Catégories INNER JOIN Produits ON Code_catégorie = Refcatégorie GROUP BY Nom_catégorie;
+SELECT Nom_catégorie, MIN(PU) AS PrixMin, MAX(PU) AS PrixMax, AVG(PU) AS PrixMoyen FROM Catégories INNER JOIN Produits ON Code_catégorie = Refcatégorie GROUP BY Nom_catégorie;
 
-SELECT Société, MAX(remise) FROM Clients, Commandes, Détails_commandes WHERE Codeclient = Refclient AND N°commande = Réfcommande GROUP BY Société;
+SELECT Société, MAX(remise) AS RemiseMax FROM Clients, Commandes, Détails_commandes WHERE Codeclient = Refclient AND N°commande = Réfcommande GROUP BY Société;
 
 ## Question 3 ##
 
 SELECT Pays, AVG(Frais_port) FROM Clients, Commandes WHERE Codeclient = Refclient GROUP BY Pays HAVING AVG(Frais_port)>500;
 
-SELECT Société, SUM(Quantité*PU) FROM Clients,Commandes, Détails_commandes WHERE Codeclient = Refclient AND N°commande = Réfcommande
+SELECT Société, SUM(Quantité*PU) AS SommeCommandes FROM Clients,Commandes, Détails_commandes WHERE Codeclient = Refclient AND N°commande = Réfcommande
 AND YEAR(Date_commande) = 2019 GROUP BY Société HAVING SUM(Quantité*PU)>3000;
 
-SELECT Société, COUNT(*) FROM Clients, Commandes WHERE Codeclient = Refclient GROUP BY Société HAVING COUNT(*)<5 ;
+SELECT Société, COUNT(*) AS NbCommandes FROM Clients, Commandes WHERE Codeclient = Refclient GROUP BY Société HAVING COUNT(*)<5 ;
